ff2a559e2426cdc8d2c3245fdac6a996b3cc6b17
seonmin5 <82851869+seonmin5@users.noreply.github.com>
2024-08-07 19:28:05 +0900

    [level 0] Title: 주사위의 개수, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.py
+++ 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.py
@@ -1,7 +1,3 @@
 def solution(box, n):
-    # 나누어 떨어지는 값 3개를 찾아 곱한 값 = 최대 개수
-    divideWidth = box[0] // n
-    divideHeight = box[1] // n
-    divideDepth = box[2] // n
-    
-    return (divideWidth*divideHeight*divideDepth)
+    x, y, z = box
+    return (x//n) * (y//n) * (z//n)
