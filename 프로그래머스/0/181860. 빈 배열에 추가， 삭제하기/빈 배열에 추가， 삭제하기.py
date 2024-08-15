573beaf898ad0e46df974e6eef5a6e0e30e0eb3a
seonmin5 <82851869+seonmin5@users.noreply.github.com>
2024-08-15 10:30:01 +0900

    [level 0] Title: 빈 배열에 추가, 삭제하기, Time: 0.07 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181860. 빈 배열에 추가， 삭제하기/빈 배열에 추가， 삭제하기.py
+++ 프로그래머스/0/181860. 빈 배열에 추가， 삭제하기/빈 배열에 추가， 삭제하기.py
@@ -3,8 +3,8 @@ def solution(arr, flag):
     
     for i in range(len(arr)):
         if flag[i]:
-            result += str(arr[i])*2*arr[i]
+            result += [arr[i]]*2*arr[i]
         else:
             result = result[:-arr[i]]
     
-    return [int(i) for i in result]
+    return result
