6e025248d7a1a3827ccbc2623b60a4709c161296
seonmin5 <82851869+seonmin5@users.noreply.github.com>
2024-08-16 10:41:22 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/README.md
+++ 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/README.md
@@ -1,6 +1,6 @@
 # [level 3] 오랜 기간 보호한 동물(1) - 59044 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/59044?language=mysql) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/59044?language=oracle) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@ Empty
 
 ### 제출 일자
 
-2024년 08월 16일 10:31:01
+2024년 08월 16일 10:41:19
 
 ### 문제 설명
 
--- 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
+++ 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -1,7 +1,10 @@
--- 코드를 입력하세요
+-- 아직 입양을 못 간 동물
+-- 가장 오래 보호소에 있던 동물 3마리 이름, 보호 시작일
+-- 보호 시작일 순으로 조회
 SELECT NAME, DATETIME
-FROM ANIMAL_INS
-WHERE ANIMAL_ID NOT IN (SELECT ANIMAL_ID
-                       FROM ANIMAL_OUTS)
-ORDER BY DATETIME
-LIMIT 3
+FROM (SELECT NAME, DATETIME
+      FROM ANIMAL_INS
+      WHERE ANIMAL_ID NOT IN (SELECT ANIMAL_ID
+                              FROM ANIMAL_OUTS)
+      ORDER BY DATETIME)
+WHERE ROWNUM <= 3
