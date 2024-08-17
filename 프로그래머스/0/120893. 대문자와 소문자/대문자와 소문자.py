d6abe9da14dd2d1cf00f06971a6b2fa336c6ea09
seonmin5 <82851869+seonmin5@users.noreply.github.com>
2024-08-17 19:03:43 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.py
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.py
@@ -1,8 +1,2 @@
 def solution(my_string):
-    answer = ''
-    for i in my_string:
-        if i.isupper():
-            answer += i.lower()
-        else:
-            answer += i.upper()
-    return answer
+    return my_string.swapcase()
