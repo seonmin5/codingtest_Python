463718815f64206e720428b461915f2c1a70d1db
seonmin5 <82851869+seonmin5@users.noreply.github.com>
2024-08-21 19:47:27 +0900

    [level 1] Title: 하샤드 수, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.py
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.py
@@ -1,8 +1,3 @@
 def solution(x):
-    sumI = 0
-    for i in str(x):
-        sumI += int(i)
-    if x % sumI == 0:
-        return True
-    else:
-        return False
+    num = (sum([int(i) for i in list(str(x))]))
+    return (x % num == 0)
