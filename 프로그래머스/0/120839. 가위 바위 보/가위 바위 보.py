a9bc778ef7a89dcf685ec52eeab032e46f8e6d3f
seonmin5 <82851869+seonmin5@users.noreply.github.com>
2024-08-21 20:09:23 +0900

    [level 0] Title: 가위 바위 보, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.py
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.py
@@ -1,10 +1,6 @@
 def solution(rsp):
-    answer = ''
+    result = ''
+    rspDict = {"2":"0", "0":"5", "5":"2"}
     for i in rsp:
-        if i == "2":
-            answer += "0"
-        elif i == "0":
-            answer += "5"
-        else:
-            answer += "2"
-    return answer
+        result += rspDict[i]
+    return result
