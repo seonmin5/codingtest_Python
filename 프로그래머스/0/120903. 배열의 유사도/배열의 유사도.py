6eff54afdad66a80f0cb752230333c0d6a2dc246
seonmin5 <82851869+seonmin5@users.noreply.github.com>
2024-08-30 10:13:05 +0900

    [level 0] Title: 배열의 유사도, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.py
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.py
@@ -1,15 +1,2 @@
 def solution(s1, s2):
-    count = 0
-    s1.sort()
-    s2.sort()
-    if len(s1) >= len(s2):
-        for i in s1:
-            if i in s2:
-                count += 1
-    if len(s1) < len(s2):
-        for i in s2:
-            if i in s1:
-                count += 1
-    return count
-                
-            
+    return len([s for s in s1 if s in s2])
