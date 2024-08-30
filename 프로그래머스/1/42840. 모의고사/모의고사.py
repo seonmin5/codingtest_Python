4b04b0caf7e80328d737a08dbf40a033bd4dc27c
seonmin5 <82851869+seonmin5@users.noreply.github.com>
2024-08-30 15:18:51 +0900

    [level 1] Title: 모의고사, Time: 6.48 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,40 +1,20 @@
 def solution(answers):
+    p1 = [1, 2, 3, 4, 5]
+    p2 = [2, 1, 2, 3, 2, 4, 2, 5]
+    p3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    count = [0, 0, 0]
     result = []
-    sol1 = [1, 2, 3, 4, 5] * len(answers)
-    sol2 = [2, 1, 2, 3, 2, 4, 2, 5] * len(answers)
-    sol3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5] * len(answers)
-    count1, count2, count3 = 0, 0, 0
     
-    # 1번~3번 수포자 문제 정답여부 확인
-    for i in range(len(answers)):
-        if sol1[i] == answers[i]:
-            count1 += 1
-    for i in range(len(answers)):
-        if sol2[i] == answers[i]:
-            count2 += 1
-    for i in range(len(answers)):
-        if sol3[i] == answers[i]:
-            count3 += 1
+    for idx, answer in enumerate(answers):
+        if answer == p1[idx%len(p1)]:
+            count[0] += 1
+        if answer == p2[idx%len(p2)]:
+            count[1] += 1
+        if answer == p3[idx%len(p3)]:
+            count[2] += 1
+
+    for idx, answer in enumerate(count):
+        if count[idx] == max(count):
+            result.append(idx+1)
     
-    # 가장 높은 점수 받은 사람(return값 오름차순)
-    if count1 == count2 == count3:
-        result.append(1)
-        result.append(2)
-        result.append(3)
-    elif count1 == count2 and count1 > count3:
-        result.append(1)
-        result.append(2)
-    elif count1 == count3 and count1 > count2:
-        result.append(1)
-        result.append(3)
-    elif count1 > count2 and count1 > count3:
-        result.append(1)
-    elif count2 == count3 and count2 > count1:
-        result.append(2)
-        result.append(3)
-    elif count2 > count3 and count2 > count1:
-        result.append(2)
-    else:
-        result.append(3)
     return result
-    
