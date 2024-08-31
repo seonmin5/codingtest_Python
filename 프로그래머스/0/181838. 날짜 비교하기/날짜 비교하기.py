0adfae7fc45e031062a3540d7d12601add9b147b
seonmin5 <82851869+seonmin5@users.noreply.github.com>
2024-08-31 10:09:13 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.py
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.py
@@ -1,9 +1,2 @@
 def solution(date1, date2):
-        if date2[0] > date1[0]:
-            return 1
-        elif date2[0] >= date1[0] and date2[1] > date1[1]:
-            return 1
-        elif date2[0] >= date1[0] and date2[1] >= date1[1] and date2[2] > date1[2]:
-            return 1
-        else:
-            return 0
+        return int(date1 < date2)
