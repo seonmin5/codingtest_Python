d590d090dc423dbcde19229af1d51aedd8834eaf
seonmin5 <82851869+seonmin5@users.noreply.github.com>
2024-11-10 15:07:44 +0900

    Title: 나눗셈의 나머지, Time: 87ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 241110/나눗셈의 나머지/remainder-of-division.java
+++ 241110/나눗셈의 나머지/remainder-of-division.java
@@ -2,6 +2,7 @@ import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
+import java.lang.Math;
 
 public class Main {
     public static void main(String[] args) throws IOException{
@@ -20,7 +21,7 @@ public class Main {
         }
 
         for (int i = 0; i < 10; i++) {
-            result += (arr[i]*arr[i]);
+            result += Math.pow(arr[i], 2);
         }
 
         System.out.print(result);
