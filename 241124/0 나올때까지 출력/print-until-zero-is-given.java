95c306d77a020bca6cabe186652d26847283ddcb
seonmin5 <82851869+seonmin5@users.noreply.github.com>
2024-11-24 17:56:46 +0900

    Title: 0 나올때까지 출력, Time: 94ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241124/0 나올때까지 출력/print-until-zero-is-given.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine().trim());
+
+        while (n != 0) {
+            System.out.println(n);
+            n = Integer.parseInt(br.readLine().trim());
+        }
+
+        br.close();
+    }
+}
